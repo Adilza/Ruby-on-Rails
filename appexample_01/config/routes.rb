e321b3b73c5a25e8abe7d75faf0af1621dea3e75
Adilza Mattos <adilzamattos@gmail.com>
2018-09-20 14:35:42 -0300

    Commit 20180920 1435

--- appexample_01/config/routes.rb
+++ appexample_01/config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  root 'welcome#index' 
   resources :customers
+  # criando uma rota 
+  get 'inicio'=>'welcome#index' 
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
